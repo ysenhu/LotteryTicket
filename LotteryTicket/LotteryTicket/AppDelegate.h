0890c5120d5088a31359a87522d3c07be7a3fdd9
徐程程 <chengcheng.xu@tqmall.com>
2014-07-19 21:47:32 +0800

    新浪彩票--建立一个空的项目

---
+++ LotteryTicket/LotteryTicket/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LotteryTicket
+//
+//  Created by xcc on 14-7-19.
+//  Copyright (c) 2014年 Hangzhou Xuanchao Technology Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LotteryTicket/LotteryTicketTests/LotteryTicketTests.m
@@ -0,0 +1,34 @@
+//
+//  LotteryTicketTests.m
+//  LotteryTicketTests
+//
+//  Created by xcc on 14-7-19.
+//  Copyright (c) 2014年 Hangzhou Xuanchao Technology Co. Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LotteryTicketTests : XCTestCase
+
+@end
+
+@implementation LotteryTicketTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
